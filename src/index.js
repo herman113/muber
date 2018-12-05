4fac8abce5fdf6d66352dd1c46db331cf562b7ca
Herman Soto <hermansoto@me.com>
2018-12-04 21:38:11 -0500

    Remove User Form and reset driver form

--- src/index.js
+++ src/index.js
@@ -1,34 +1,18 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import unsplash from './api/unsplash';
-// import DriverForm from './components/DriverForm';
-import UserForm from './components/UserForm';
+import DriverForm from './components/DriverForm';
 
 import '../style/style.css';
 
 export default class App extends Component {
   constructor(props) {
     super(props);
-    this.onUserFormSubmit = this.onUserFormSubmit.bind(this)
-    this.state = {images: []}
   }
-  async onUserFormSubmit(firstName) {
-    const response = await unsplash.get('/search/photos', {
-      params: { query: firstName },
-    })
-  
-    this.setState({ images: response.data.results });
-  }
+
   render() {
     return (
       <div className="ui container" style={{"marginTop": "20px"}}>
-        <UserForm onUserFormSubmit={this.onUserFormSubmit}/>
-        <ul>
-          {this.state.images.map((image, index) => {
-            return <li key={index}><img src={image.urls.regular} style={{"width": "300", "float":"left"}}/></li>
-          })}
-        </ul>
-        {/* <DriverForm/> */}
+        <DriverForm/>
       </div>
     );
   }
